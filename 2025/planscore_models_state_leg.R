ff557af96a438b999b9405c5a74a06c992c858fe
Michal Migurski <mike@teczno.com>
2026-03-19 16:34:21 -0700

    Upping chain count to match cores and upping iteration count

--- 2025/planscore_models_state_leg.R
+++ 2025/planscore_models_state_leg.R
@@ -73,8 +73,8 @@ m <- brm(bf(dem_share_imputed ~ dpres_mn + incumb +
                            group="stateabrev"),
                  set_prior("student_t(3, 0.02, 0.05)", class="sd",coef="incumb",
                            group="stateabrev")),
-         cores=detectCores(), chains=4, control=list(adapt_delta=0.99999, max_treedepth=12),
-         warmup=20, iter=60, refresh=10, thin=16) #warmup and iter for number of cycles
+         cores=detectCores(), chains=detectCores(), control=list(adapt_delta=0.99999, max_treedepth=12),
+         warmup=2000, iter=6000, refresh=10, thin=16) #warmup and iter for number of cycles
 proc.time() - start
 saveRDS(m, "full_model_2026A_incumbency_statelege.rds")
 
@@ -128,8 +128,8 @@ m <- brm(bf(dem_share_imputed ~ dpres_mn +
                            group="stateabrev"),
                  set_prior("student_t(3, 0.11, 0.1)", class="sd",coef="dpres_mn",
                            group="stateabrev")),
-         cores=detectCores(), chains=4, control=list(adapt_delta=0.99999, max_treedepth=12),
-         warmup=20, iter=60, refresh=10, thin=16) #warmup and iter for number of cycles
+         cores=detectCores(), chains=detectCores(), control=list(adapt_delta=0.99999, max_treedepth=12),
+         warmup=2000, iter=6000, refresh=10, thin=16) #warmup and iter for number of cycles
 proc.time() - start
 saveRDS(m, "full_model_2026A_openseat_statelege.rds")
 
